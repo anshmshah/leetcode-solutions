435c6f2f45b1ae2a9daccb6e4e1a41f2cde08d66
Ansh shah <85230278+anshmshah@users.noreply.github.com>
2026-07-13 22:03:26 +0530

    ✅ Solve: Add Two Numbers (Java)

--- Medium/add-two-numbers/add-two-numbers.java
+++ Medium/add-two-numbers/add-two-numbers.java
@@ -1,17 +1,39 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
-    public boolean isPalindrome(int x) {
-        if (x < 0)
-            return false;
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode current = dummy;
 
-        int original = x;
-        long reverse = 0;
+        int carry = 0;
 
-        while (x != 0) {
-            int digit = x % 10;
-            reverse = reverse * 10 + digit;
-            x /= 10;
+        while (l1 != null || l2 != null || carry != 0) {
+
+            int sum = carry;
+
+            if (l1 != null) {
+                sum += l1.val;
+                l1 = l1.next;
+            }
+
+            if (l2 != null) {
+                sum += l2.val;
+                l2 = l2.next;
+            }
+
+            carry = sum / 10;
+            current.next = new ListNode(sum % 10);
+            current = current.next;
         }
 
-        return original == reverse;
+        return dummy.next;
     }
 }
